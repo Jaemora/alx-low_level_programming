470cf2cf52aa7cc0acac38592d5d662c60fb7f71
Jaemora <jamalitos64@gmail.com>
2023-02-23 05:30:20 -0800

    jk

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,15 +7,10 @@
 
 int main(void)
 {
- int i;
- /**
- *  while (i < 10)
- *       {
- *              putchar(i);
- *       }
- */
- i = 0;
- positive_or_negative(i);
+	int i;
+ 
+	i = 98;
+	positive_or_negative(i);
 
-return (0);
+	return (0);
 }
