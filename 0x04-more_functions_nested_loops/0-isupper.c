7a52110899347c2fdf1c17d5afcdc4db2501ce02
Jaemora <jamalitos64@gmail.com>
2023-02-23 15:23:41 -0800

    jk

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,20 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1 and On error 0 is returned.
- */
-
-int _isupper(int x)
-
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
+*/
+int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+
+
+ if (c >= 'A' && c <= 'Z')
+ {
+ return (1);
+ }
+ else
+{
+ return (0);
+}
+
 }
