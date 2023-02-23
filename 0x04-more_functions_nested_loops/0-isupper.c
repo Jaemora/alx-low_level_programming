b2dc5a76340d5948a1e5dd0102bb9a35dff79199
Jaemora <jamalitos64@gmail.com>
2023-02-23 10:39:47 -0800

    jk

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,15 +6,12 @@
 */
 int _isupper(int c)
 {
-
-
- if (c >= 65 && c <= 98)
- {
- return (1);
- }
- else
-{
- return (0);
-}
-
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
