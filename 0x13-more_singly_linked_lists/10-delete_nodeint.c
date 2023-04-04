66ca9cbc18ac520539475a688e34ed7707105535
Jaemora <jamalitos64@gmail.com>
2023-04-04 10:08:15 -0700

    jk

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+* delete_nodeint_at_index - deletes  nodes 
+* @head: variable
+* @index: variable
+* Return: output
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+listint_t *tp = *head;
+listint_t *now = NULL;
+unsigned int z = 0;
+
+if (*head == NULL)
+return (-1);
+if (index == 0)
+{
+*head = (*head)->next;
+free(tp);
+return (1);
+}
+while (z < index - 1)
+{
+if (!tp || !(tp->next))
+return (-1);
+tp = tp->next;
+z++;
+}
+now = tp->next;
+tp->next = now->next;
+free(now);
+return (1);
+}
