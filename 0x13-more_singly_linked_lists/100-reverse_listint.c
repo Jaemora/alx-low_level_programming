cf882c743e5562bf76ff0e0d5983828d9516fd2b
Jaemora <jamalitos64@gmail.com>
2023-04-04 15:28:48 -0700

    jk

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,16 +8,16 @@
 */
 listint_t *reverse_listint(listint_t **head)
 {
-listint_t *prev = NULL;
-listint_t *next = NULL;
+listint_t *rev = NULL;
+listint_t *nx = NULL;
 
 while (*head)
 {
-next = (*head)->next;
+nx = (*head)->next;
 (*head)->next = prev;
-prev = *head;
-*head = next;
+rev = *head;
+*head = nx;
 }
-*head = prev;
+*head = rev;
 return (*head);
 }
